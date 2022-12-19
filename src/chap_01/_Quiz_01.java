88cca93e65a945344531e4a48cb151969ef1a551
KKYUNG2 <backender9615@gmail.com>
2022-12-19 22:31:50 +0900

    #자바공부하기 1.나도코딩 따라히기 2일차

--- src/chap_01/_Quiz_01.java
+++ src/chap_01/_Quiz_01.java
@@ -13,10 +13,10 @@ public class _Quiz_01 {
         String busNo = "상암08";
 
         // 남은 시간 (3분, 5분)
-        int minute = 3;
+        int minute = 5;
 
         // 남은 거리 (1.5km, 0.8km)
-        double distance = 1.2;
+        double distance = 0.8;
 
         System.out.println(busNo + "번 버스");
         System.out.println("약 " + minute + "분 후 도착");
